29caae9321a85ed688cba0e9a5f97d93c0ea370c
GameDevJ180 <josesielo1805@gmail.com>
2025-09-08 11:03:01 -0600

    👷

--- Assets/Scripts/Movement Player.cs
+++ Assets/Scripts/Movement Player.cs
@@ -9,38 +9,62 @@ using UnityEngine.InputSystem;
 
 public class MovementPlayer : MonoBehaviour
 {
-
-    float movement = 0f;
-    float jump = 1f;
-    public float speed = 0f;
+    [Header("Movement Settings")]
+    [SerializeField] float JumpForce = 5f;
+    [SerializeField] float Speed = 5f;
+    [SerializeField] float DashForce = 5f;
+    bool isGrounded = false;
+    [Header("Essential Components")]
     [SerializeField] Rigidbody2D rb;
+    Vector2 Direction;
+    public Animator animator;
 
     void Start()
     {
         rb.GetComponent<Rigidbody2D>();
+        animator.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = 0;
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(Vector2.left);
+            Direction = Vector2.left;
+            rb.AddForce(Direction * Speed);
+            animator.SetBool("DirectionState", false);
         }
-        
+
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(Vector2.right);
+            Direction = Vector2.right;
+            rb.AddForce(Direction * Speed);
+            animator.SetBool("DirectionState", true);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(Vector2.up);
+            rb.AddForce(Vector2.up * JumpForce * 100f);
         }
-
-        Vector2 moveX = new Vector2(movement * Time.deltaTime * speed, 0);
-        Vector2 jump = new Vector2(0f, 1f);
-        transform.position += (Vector3)moveX;
+        if(Input.GetKeyDown(KeyCode.LeftShift) && Direction == Vector2.right)
+        {
+            rb.AddForce(Vector2.right * DashForce * 100f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Direction == Vector2.left)
+        {
+            rb.AddForce(Vector2.left * DashForce * 100f);
+        }
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
 }
